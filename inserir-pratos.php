eb22c9e7512abcb719b2b96ffb7f154333e1a764
eduardopaiva420 <eduardok86@gmail.com>
2020-12-03 21:18:14 -0300

    inicio do projeto

---
+++ inserir-pratos.php
@@ -0,0 +1,185 @@
+    
+   
+    <?php
+
+        $pratos = array (
+
+            array (
+                'codigo' => 'jardim-cogumelos',
+                'nome' => 'Jardim de Cogumelos',
+                'categoria' => 'Entradas',
+                'descr' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales justo eu mauris tincidunt, id dignissim magna elementum. Sed euismod efficitur tortor eu facilisis. Proin augue nunc, luctus hendrerit velit sit amet, iaculis porta velit. In vulputate tristique urna. Praesent tempus ipsum augue, sit amet tristique lacus semper cursus.',
+                'preco' => 12.00,
+                'calorias' => 290,
+                'destaque' => 0
+
+            ),
+
+            array (
+                'codigo' => 'camarao-alho',
+                'nome' => 'Camarões Ao Alho',
+                'categoria' => 'Entradas',
+                'descr' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales justo eu mauris tincidunt, id dignissim magna elementum. Sed euismod efficitur tortor eu facilisis. Proin augue nunc, luctus hendrerit velit sit amet, iaculis porta velit. In vulputate tristique urna. Praesent tempus ipsum augue, sit amet tristique lacus semper cursus.',
+                'preco' => 16.00,
+                'calorias' => 340,
+                'destaque' => 1
+
+            ),
+
+            array (
+                'codigo' => 'salada-grega',
+                'nome' => 'Salada Grega',
+                'categoria' => 'Entradas',
+                'descr' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales justo eu mauris tincidunt, id dignissim magna elementum. Sed euismod efficitur tortor eu facilisis. Proin augue nunc, luctus hendrerit velit sit amet, iaculis porta velit. In vulputate tristique urna. Praesent tempus ipsum augue, sit amet tristique lacus semper cursus.',
+                'preco' => 11.00,
+                'calorias' => 180,
+                'destaque' => 0
+
+            ),
+
+            array (
+                'codigo' => 'brie-geleia',
+                'nome' => 'Tapas de Queijo Brie e Geleia',
+                'categoria' => 'Entradas',
+                'descr' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales justo eu mauris tincidunt, id dignissim magna elementum. Sed euismod efficitur tortor eu facilisis. Proin augue nunc, luctus hendrerit velit sit amet, iaculis porta velit. In vulputate tristique urna. Praesent tempus ipsum augue, sit amet tristique lacus semper cursus.',
+                'preco' => 14.00,
+                'calorias' => 400,
+                'destaque' => 0
+
+            ),
+
+            array (
+                'codigo' => 'picanha-brasileira',
+                'nome' => 'Picanha à Brasileira',
+                'categoria' => 'Pratos Principais',
+                'descr' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales justo eu mauris tincidunt, id dignissim magna elementum. Sed euismod efficitur tortor eu facilisis. Proin augue nunc, luctus hendrerit velit sit amet, iaculis porta velit. In vulputate tristique urna. Praesent tempus ipsum augue, sit amet tristique lacus semper cursus.',
+                'preco' => 28.00,
+                'calorias' => 890,
+                'destaque' => 1
+
+            ),
+
+             array (
+                'codigo' => 'costelinha',
+                'nome' => 'Costelinha de Porco',
+                'categoria' => 'Pratos Principais',
+                'descr' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales justo eu mauris tincidunt, id dignissim magna elementum. Sed euismod efficitur tortor eu facilisis. Proin augue nunc, luctus hendrerit velit sit amet, iaculis porta velit. In vulputate tristique urna. Praesent tempus ipsum augue, sit amet tristique lacus semper cursus.',
+                'preco' => 29.00,
+                'calorias' => 930,
+                'destaque' => 0
+
+            ),
+
+            array (
+                'codigo' => 'salmao-legumes',
+                'nome' => 'Salmão Aos Legumes',
+                'categoria' => 'Pratos Principais',
+                'descr' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales justo eu mauris tincidunt, id dignissim magna elementum. Sed euismod efficitur tortor eu facilisis. Proin augue nunc, luctus hendrerit velit sit amet, iaculis porta velit. In vulputate tristique urna. Praesent tempus ipsum augue, sit amet tristique lacus semper cursus.',
+                'preco' => 29.00,
+                'calorias' => 690,
+                'destaque' => 1
+
+            ),
+
+            array (
+                'codigo' => 'churrasco-misto',
+                'nome' => 'Churrasco Misto',
+                'categoria' => 'Pratos Principais',
+                'descr' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales justo eu mauris tincidunt, id dignissim magna elementum. Sed euismod efficitur tortor eu facilisis. Proin augue nunc, luctus hendrerit velit sit amet, iaculis porta velit. In vulputate tristique urna. Praesent tempus ipsum augue, sit amet tristique lacus semper cursus.',
+                'preco' => 26.00,
+                'calorias' => 700,
+                'destaque' => 0
+
+            ),
+
+            array (
+                'codigo' => 'cheesecake-cereja',
+                'nome' => 'Cheese Cake de Cereja',
+                'categoria' => 'Sobremesas',
+                'descr' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales justo eu mauris tincidunt, id dignissim magna elementum. Sed euismod efficitur tortor eu facilisis. Proin augue nunc, luctus hendrerit velit sit amet, iaculis porta velit. In vulputate tristique urna. Praesent tempus ipsum augue, sit amet tristique lacus semper cursus.',
+                'preco' => 16.00,
+                'calorias' => 680,
+                'destaque' => 1
+
+            ),
+
+            array (
+                'codigo' => 'flan-frutas-vermelhas',
+                'nome' => 'Flan de Frutas Vermelhas',
+                'categoria' => 'Sobremesas',
+                'descr' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales justo eu mauris tincidunt, id dignissim magna elementum. Sed euismod efficitur tortor eu facilisis. Proin augue nunc, luctus hendrerit velit sit amet, iaculis porta velit. In vulputate tristique urna. Praesent tempus ipsum augue, sit amet tristique lacus semper cursus.',
+                'preco' => 14.00,
+                'calorias' => 620,
+                'destaque' => 0
+
+            ),
+
+            array (
+                'codigo' => 'petit-gateau',
+                'nome' => 'Petit Gateau',
+                'categoria' => 'Sobremesas',
+                'descr' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales justo eu mauris tincidunt, id dignissim magna elementum. Sed euismod efficitur tortor eu facilisis. Proin augue nunc, luctus hendrerit velit sit amet, iaculis porta velit. In vulputate tristique urna. Praesent tempus ipsum augue, sit amet tristique lacus semper cursus.',
+                'preco' => 19.00,
+                'calorias' => 720,
+                'destaque' => 0
+
+            ),
+
+            array (
+                'codigo' => 'creme-papaya',
+                'nome' => 'Creme de Papaya com Cassis',
+                'categoria' => 'Sobremesas',
+                'descr' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales justo eu mauris tincidunt, id dignissim magna elementum. Sed euismod efficitur tortor eu facilisis. Proin augue nunc, luctus hendrerit velit sit amet, iaculis porta velit. In vulputate tristique urna. Praesent tempus ipsum augue, sit amet tristique lacus semper cursus.',
+                'preco' => 15.00,
+                'calorias' => 520,
+                'destaque' => 0
+
+            )
+
+
+        );
+
+    ?>
+
+
+    <?php
+
+        $server = 'localhost';
+        $user = 'root';
+        $password = 'root';
+        $db_name = 'restaurante';
+        $port = '8889';
+
+        $db_connect = new mysqli($server,$user,$password,$db_name,$port);
+        mysqli_set_charset($db_connect,"utf8");
+
+        if ($db_connect->connect_error) {
+            echo 'Falha: ' . $db_connect->connect_error;
+        } else {
+            echo 'Conexão feita com sucesso' . '<br><br>';
+
+            foreach($pratos as $prato){
+                $codigo = $prato['codigo'];
+                $nome = $prato['nome'];
+                $categoria = $prato['categoria'];
+                $descr = $prato['descr'];
+                $preco = $prato['preco'];
+                $calorias = $prato['calorias'];
+                $destaque = $prato['destaque'];
+                
+                $sql = "INSERT into pratos(codigo, nome, categoria, descricao, preco, calorias, destaques)
+                value('$codigo', '$nome', '$categoria', '$descr', '$preco', '$calorias', '$destaque')";
+            
+                if($db_connect->query($sql)) {
+                    echo $nome . " inserido com sucesso" . '<br><br>';
+                }else{
+                    echo "Não foi possivel inserir " . $nome . '<br>';
+                    echo mysqli_error($db_connect) . '<br><br>';
+                }
+                 echo '<br>';
+            }
+            
+        }
+    ?>   
+
+ 
